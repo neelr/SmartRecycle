1e3d192c072825bce348a26598fe77fc3f5c435b
Hacker719 <neel.redkar@gmail.com>
2019-03-14 15:39:23 -0700

    Initial Commit

---
+++ RecycleSmartV2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecycleSmartV2
+//
+//  Created by Neel Redkar on 3/14/19.
+//  Copyright © 2019 tecton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
