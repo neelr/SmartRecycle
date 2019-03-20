09dc5737ff92782de39b184da16fc3c1a9f2158d
Hacker719 <neel.redkar@gmail.com>
2019-03-19 21:33:11 -0700

    init

--- RecycleSmartV2/ViewController.swift
+++ RecycleSmartV2/ViewController.swift
@@ -1,20 +1,80 @@
 //
 //  ViewController.swift
-//  RecycleSmartV2
 //
-//  Created by Neel Redkar on 3/14/19.
-//  Copyright © 2019 tecton. All rights reserved.
+//  Created by Tecton on 1/12/19.
+//  Copyright © 2019 Tecton All rights reserved.
 //
 
 import UIKit
+import AVKit
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    @IBOutlet weak var imageframe: UIView!
+    
+    @IBOutlet weak var identifierLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = imageframe.frame
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(dataOutput)
+        
+        
+        setupIdentifierConfidenceLabel()
     }
-
-
+    
+    fileprivate func setupIdentifierConfidenceLabel() {
+        view.addSubview(identifierLabel)
+        identifierLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -32).isActive = true
+        identifierLabel.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        identifierLabel.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        identifierLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        usleep(1000000)
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        
+        guard let model = try? VNCoreMLModel(for: RecycleSmartV2().model) else { return }
+        let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
+            
+            
+            guard let results = finishedReq.results as? [VNClassificationObservation] else { return }
+            
+            guard let firstObservation = results.first else { return }
+            
+            print(firstObservation.identifier, firstObservation.confidence)
+            DispatchQueue.main.async {
+                if (firstObservation.confidence<0.3) {
+                    self.identifierLabel.text = "trash  \(Int(firstObservation.confidence * 100))%"
+                } else if (firstObservation.confidence<0.2) {
+                    self.identifierLabel.text = "Undefined? \(Int(firstObservation.confidence * 100))%"
+                } else {
+                    self.identifierLabel.text = "\(firstObservation.identifier) \(Int(firstObservation.confidence * 100))%"
+                }
+            }
+            
+        }
+        
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
+    
 }
 
---
+++ RecycleSmartV2/ViewController2.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController2.swift
+//  RecycleSmartV2
+//
+//  Created by Neel Redkar on 3/19/19.
+//  Copyright © 2019 tecton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+    @IBAction func action(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
